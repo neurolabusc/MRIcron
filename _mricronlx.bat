d5c85c511a17bf0e1c6c0c498a6b254f451ff504
Chris Rorden <rorden@mailbox.sc.edu>
2018-06-15 15:09:06 -0400

    Linux build latest dcm2niix

--- _mricronlx.bat
+++ _mricronlx.bat
@@ -22,8 +22,14 @@ cp ./MRIcron ~/mricron_lx
 # cp ./npm/npm ~/mricron_lx/npm32
 # cp ./dcm2nii/dcm2niigui ~/mricron_lx/dcm2niigui32
 
-
 ./_xclean.bat
+
+#for dcm2niix
+cd ~/dcm2niix
+git pull
+cd ~/dcm2niix/console
+make
+cp dcm2niix ~/mricron_lx/dcm2niix
 
 cd ~
 zip -r ~/mricron_lx.zip mricron_lx
